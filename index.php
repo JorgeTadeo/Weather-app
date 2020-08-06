3ecbee5a2fa6cf8efc96794cbff1059208fa281b
JorgeTadeo <jorge_tadeo@icloud.com>
2020-08-06 14:51:43 -0700

    added view port meta tag

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 <html>
   <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Weather app</title>
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.1/css/bootstrap.min.css" integrity="sha384-VCmXjywReHh4PwowAiWNagnWcLhlEJLA5buUprzK8rxFgeH0kww/aWY76TfkUoSX" crossorigin="anonymous">
 	<script src="apifetcher.js" async></script>
